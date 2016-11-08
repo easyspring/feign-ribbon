e71df9523b5d8071c5823ebb71ef3c748d40e661
胡家宝 <jiabao_hu@corp.netease.com>
2016-11-08 17:12:01 +0800

    hystrix, callback

--- src/main/java/com/netease/services/ComputeClient.java
+++ src/main/java/com/netease/services/ComputeClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("compute-service-me")
+@FeignClient(value = "compute-service-me",fallback= ComputeClientHystrix.class)
 public interface ComputeClient {
 	@RequestMapping(method=RequestMethod.GET, value="/add")
 	Integer add(@RequestParam(value="a") Integer a,@RequestParam(value="b") Integer b);
